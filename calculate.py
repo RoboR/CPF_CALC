389fe9e0d6c85627d08d90a37b4ae53824b47d54
RoboR <rusty.robor@gmail.com>
2025-10-06 10:24:03 +0800

    - Beautify message logs - Add cpf topup - Add tax relief calc

--- calculate.py
+++ calculate.py
@@ -3,8 +3,7 @@ from datetime import timedelta
 import calendar
 import math
 
-
-#FIXED
+#  ==========================     FIXED    ==========================
 # https://www.cpf.gov.sg/service/article/what-are-the-cpf-allocation-rates-for-2025
 CPF_CONTIBUTION_RATES_35_AND_BELOW = {"OA" : 0.6217, "SA" : 0.1621, "MA" : 0.2162}
 CPF_CONTIBUTION_RATES_36_TO_45 = {"OA" : 0.5677, "SA" : 0.1891, "MA" : 0.2432}
@@ -60,11 +59,28 @@ def GenerateContributionRatesDict(startWorkingAge, endWorkingAge):
     
 CPF_ACCOUNT_ALLOCATION_RATE, CPF_CONTRIBUTION_RATE = GenerateContributionRatesDict(13, 100)
 
-
 CPF_MA_MAX_AMOUNT = 75500  #https://www.cpf.gov.sg/service/article/is-there-a-maximum-amount-that-i-can-save-in-my-medisave-account
 FRS_MAX_AMOUNT = 228200    # https://www.cpf.gov.sg/service/article/how-much-is-my-full-retirement-sum
 CPF_MONTHLY_CEILING = 8000
 CPF_ANNUAL_LIMIT = 37740 # https://www.cpf.gov.sg/service/article/what-is-the-cpf-annual-limit
+
+
+
+
+def GetNextMonthEndDate(readDateTime):
+    # Get end date of this month
+    readDateTime = readDateTime.replace(
+    day=calendar.monthrange(readDateTime.year, readDateTime.month)[1])
+    
+    # First day of next month 
+    readDateTime += timedelta(days=1)
+    
+    # Get end date of next month
+    readDateTime = readDateTime.replace(
+    day=calendar.monthrange(readDateTime.year, readDateTime.month)[1])
+    
+    return readDateTime
+
 
 # https://www.cpf.gov.sg/service/article/how-much-extra-interest-can-i-earn-on-my-cpf-savings
 def CalculateMonthlyCPFInterest(age, OABalance, SABalance, MABalance):
@@ -99,36 +115,7 @@ def CalculateMonthlyCPFInterest(age, OABalance, SABalance, MABalance):
     maInterest += (MABalance * MA_INTEREST_RATE / 12)
     
     return round(oaInterest, 2), round(saInterest, 2), round(maInterest, 2)
-
-def GetNextMonthEndDate(readDateTime):
-    # Get end date of this month
-    readDateTime = readDateTime.replace(
-    day=calendar.monthrange(readDateTime.year, readDateTime.month)[1])
     
-    # First day of next month 
-    readDateTime += timedelta(days=1)
-    
-    # Get end date of next month
-    readDateTime = readDateTime.replace(
-    day=calendar.monthrange(readDateTime.year, readDateTime.month)[1])
-    
-    return readDateTime
-    
-def GetMonthCpfContributions(salary, age, prContributionRate=None):
-    allocationRate = CPF_ACCOUNT_ALLOCATION_RATE[age]
-    employerRate, employeeRate = prContributionRate if (prContributionRate) else CPF_CONTRIBUTION_RATE[age]
-    
-    cpfSalary = min(salary, CPF_MONTHLY_CEILING)
-    
-    employerContribution = (round(cpfSalary * employerRate * allocationRate['OA'], 2),
-                            round(cpfSalary * employerRate * allocationRate['SA'], 2),
-                            round(cpfSalary * employerRate * allocationRate['MA'], 2))
-    
-    employeeContribution = (round(cpfSalary * employeeRate * allocationRate['OA'], 2),
-                            round(cpfSalary * employeeRate * allocationRate['SA'], 2),
-                            round(cpfSalary * employeeRate * allocationRate['MA'], 2))
-    
-    return employerContribution, employeeContribution
     
 def GetBonusContributions(bonus, age, yearlyCpfContribution):
     allocationRate = CPF_ACCOUNT_ALLOCATION_RATE[age]
@@ -147,6 +134,26 @@ def GetBonusContributions(bonus, age, yearlyCpfContribution):
     
     return employerContribution, employeeContribution
 
+
+def GetMonthlyCpfContributions(salary, age, prContributionRate=None):
+    allocationRate = CPF_ACCOUNT_ALLOCATION_RATE[age]
+    employerRate, employeeRate = prContributionRate if (prContributionRate) else CPF_CONTRIBUTION_RATE[age]
+    
+    cpfSalary = min(salary, CPF_MONTHLY_CEILING)
+    
+    employerContribution = (round(cpfSalary * employerRate * allocationRate['OA'], 2),
+                            round(cpfSalary * employerRate * allocationRate['SA'], 2),
+                            round(cpfSalary * employerRate * allocationRate['MA'], 2))
+    
+    employeeContribution = (round(cpfSalary * employeeRate * allocationRate['OA'], 2),
+                            round(cpfSalary * employeeRate * allocationRate['SA'], 2),
+                            round(cpfSalary * employeeRate * allocationRate['MA'], 2))
+                            
+    totalContribution = tuple(round(sum(total), 2) for total in zip(employerContribution, employeeContribution))
+    
+    return employerContribution, employeeContribution, totalContribution
+
+
 def AdjustCpfAccounts(oaAmount, saAmount, maAmount):
     overFlowAmount = max(0, (maAmount - CPF_MA_MAX_AMOUNT))
     if (not overFlowAmount):
@@ -161,18 +168,49 @@ def AdjustCpfAccounts(oaAmount, saAmount, maAmount):
     oaAmount += overFlowAmount
     
     return oaAmount, saAmount, maAmount
+    
 
-# USER SETTINGS
+#  ==========================     USER SETTINGS    ==========================
+
 USER_BIRTHDATE = datetime(1990, 10, 4)
+# USER_PR_OBTAIN_DATE = datetime(2022, 1, 24)
 USER_PR_OBTAIN_DATE = datetime(2017, 4, 10)
-currentDate = datetime(2025, 8, 1)#datetime.now()
+currentDate = datetime(2025, 8, 1)
+# currentDate = datetime.now()
 RETIRE_DATE =  datetime(2055, 10, 5)
 
 MONTHLY_INCOME = 6500
-CPF_OA_BALANCE = 72104.46
-CPF_SA_BALANCE = 31707.52
-CPF_MA_BALANCE = 33600.99
+CPF_OA_BALANCE = 70095.43
+CPF_SA_BALANCE = 31182
+CPF_MA_BALANCE = 32900.08
 
+HAS_AWS = True
+FIRST_YEAR_AWS_AMOUNT = 0
+
+YEARLY_BONUS = 2    # months
+FIRST_YEAR_BONUS_AMOUNT = 0
+
+YEARLY_INCREMENT_RATE = 0.04    # percentage
+CPF_TOPUPS = \
+{ # month : {OA, SA, MA}
+    1 : (0, 0, 8000), 
+    2 : (0, 0, 0), 
+    3 : (0, 0, 0), 
+    4 : (0, 0, 0), 
+    5 : (0, 0, 0), 
+    6 : (0, 0, 0), 
+    7 : (0, 0, 0), 
+    8 : (0, 0, 0), 
+    9 : (0, 0, 0), 
+    10 : (0, 0, 0),  
+    11 : (0, 0, 0), 
+    12 : (0, 0, 0)
+}
+TAX_RELIEF_AMOUNT = 3000 + 1000
+
+
+
+#  ==========================    Code variables    ==========================
 endOfTheMonth = currentDate
 salary = MONTHLY_INCOME
 oaAmount = CPF_OA_BALANCE
@@ -180,13 +218,10 @@ saAmount = CPF_SA_BALANCE
 maAmount = CPF_MA_BALANCE
 yearlyTaxableIncome = 0
 yearlyCpfContribution = 0
-
-HAS_AWS = True
-YEARLY_BONUS = 2
 grossSalary = 0
 preYearGrosSalary = 0
 awsAmount = 0
-yearlyIncrementRate = 0.03
+cpfTopupReliefAmount = 0
 
 
 # Main loop calculation
@@ -194,8 +229,10 @@ while (((RETIRE_DATE - endOfTheMonth).days) >= 30):
     userAge = int((endOfTheMonth - USER_BIRTHDATE).days / 365)
     
     if endOfTheMonth.month == 1:
-        print("\nLast year taxable income =", round(yearlyTaxableIncome, 2),
-              "; CPF total contribuition =", round(yearlyCpfContribution, 2))
+        print("\n******** SUMMARY ********\nLast year taxable income (after deduction) =", 
+        round(yearlyTaxableIncome, 2) - TAX_RELIEF_AMOUNT - cpfTopupReliefAmount,
+        "; CPF total contribuition =", round(yearlyCpfContribution, 2), "\n*************************")
+        
         yearlyTaxableIncome = 0
         preYearGrosSalary = grossSalary
         grossSalary = 0
@@ -219,7 +256,7 @@ while (((RETIRE_DATE - endOfTheMonth).days) >= 30):
         elif (prYear <= 2):
            prContributionRate = CONTRIUBTION_RATE_PR_SECOND_YEAR
            
-    employerContribution, employeeContribution = GetMonthCpfContributions(salary, userAge, prContributionRate)
+    employerContribution, employeeContribution, totalContribution = GetMonthlyCpfContributions(salary, userAge, prContributionRate)
     
     oaAmount = round(oaAmount + oaInterest + employerContribution[0] + employeeContribution[0], 2)
     saAmount = round(saAmount + saInterest + employerContribution[1] + employeeContribution[1], 2)
@@ -231,14 +268,34 @@ while (((RETIRE_DATE - endOfTheMonth).days) >= 30):
     
     oaAmount, saAmount, maAmount = AdjustCpfAccounts(oaAmount, saAmount, maAmount)
     
+    # CPF Topups
+    oaTopUp, saTopUp, maTopUp = CPF_TOPUPS[endOfTheMonth.month]
+    topUpMessage = "Top Up Amount : " if (oaTopUp or saTopUp or maTopUp) else None
+    if oaTopUp:
+        topUpMessage += ("OA = " + str(oaTopUp) + " ")
+    if saTopUp:
+        topUpMessage += ("SA = " + str(saTopUp) + " ")
+    if maTopUp:
+        topUpMessage += ("MA = " + str(maTopUp) + " ")
+    
+    if topUpMessage:
+        print(topUpMessage)
+        
+        
+    oaAmount += oaTopUp
+    saAmount += saTopUp
+    maAmount += maTopUp
+    cpfTopupReliefAmount += (saAmount + maAmount)
+    
     # AWS Bonus
     if HAS_AWS and endOfTheMonth.month == 12:
-        awsAmount = round(grossSalary / 12, 2)
+        awsAmount = FIRST_YEAR_AWS_AMOUNT if FIRST_YEAR_AWS_AMOUNT else round(grossSalary / 12, 2)
         awsEmployerContribution, awsEmployeeContribution = GetBonusContributions(awsAmount, userAge, yearlyCpfContribution)
         oaAmount =  round(oaAmount + awsEmployerContribution[0] + awsEmployeeContribution[0], 2)
         saAmount =  round(saAmount + awsEmployerContribution[1] + awsEmployeeContribution[1], 2)
         maAmount =  round(maAmount + awsEmployerContribution[1] + awsEmployeeContribution[2], 2)
-        print("AWS : ", awsAmount, ";CPF", awsEmployerContribution, awsEmployeeContribution)
+        print("###### AWS ######\n", awsAmount, "; CPF", awsEmployerContribution, awsEmployeeContribution,
+        "\n#################")
 
         yearlyTaxableIncome += (awsAmount - awsEmployeeContribution[0] - awsEmployeeContribution[1] - awsEmployeeContribution[2])
         yearlyCpfContribution += (awsEmployerContribution[0] + awsEmployerContribution[1] + awsEmployerContribution[2] +
@@ -246,11 +303,12 @@ while (((RETIRE_DATE - endOfTheMonth).days) >= 30):
         oaAmount, saAmount, maAmount = AdjustCpfAccounts(oaAmount, saAmount, maAmount)
         
     # Yearly Bonus
-    elif YEARLY_BONUS and endOfTheMonth.month == 3:
-        yearlyBonus = (preYearGrosSalary / 12) * YEARLY_BONUS
+    if YEARLY_BONUS and endOfTheMonth.month == 6:
+        yearlyBonus = FIRST_YEAR_BONUS_AMOUNT if FIRST_YEAR_BONUS_AMOUNT else round((preYearGrosSalary / 12) * YEARLY_BONUS, 2)
         
         bonusEmployerContribution, bonusEmployeeContribution = GetBonusContributions(yearlyBonus, userAge, yearlyCpfContribution)
-        print("!!!!!BONUS!!!!! : ", yearlyBonus, ";CPF", bonusEmployerContribution, bonusEmployeeContribution)
+        print("$$$$$  BONUS  $$$$$\n", yearlyBonus, "; CPF contriubtion :", bonusEmployerContribution, bonusEmployeeContribution,
+        "\n$$$$$$$$$$$$$$$$$$$")
 
         oaAmount =  round(oaAmount + bonusEmployerContribution[0] + bonusEmployeeContribution[0], 2)
         saAmount =  round(saAmount + bonusEmployerContribution[1] + bonusEmployeeContribution[1], 2)
@@ -262,14 +320,13 @@ while (((RETIRE_DATE - endOfTheMonth).days) >= 30):
         oaAmount, saAmount, maAmount = AdjustCpfAccounts(oaAmount, saAmount, maAmount)
     
     # Increment
-    elif yearlyIncrementRate and endOfTheMonth.month == 7:
-        salary += (salary * yearlyIncrementRate)
-        
-    # Add CPF volunter contribution and tax deduction
+    if YEARLY_INCREMENT_RATE and endOfTheMonth.month == 6:
+        salary += (salary * YEARLY_INCREMENT_RATE)
         
     print(endOfTheMonth.strftime("%Y-%m-%d"), 
     "; Age", userAge, 
     "; Salary ", salary,
+    ";Total CPF :", totalContribution,
     "; EMPLOYER CPF :", employerContribution, 
     "; EMPLOYEE CPF :", employeeContribution, 
     "; CPF Interest :", oaInterest, saInterest, maInterest,
@@ -277,6 +334,7 @@ while (((RETIRE_DATE - endOfTheMonth).days) >= 30):
     "; taxable income:", round(yearlyTaxableIncome, 2))
     
     endOfTheMonth = GetNextMonthEndDate(endOfTheMonth)
+    
     
 
 
